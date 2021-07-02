2217d455856001661bac7d075b48c2ae3f418e00
hrushi-thorat <hrushikeshst54@gmail.com>
2021-07-02 02:52:49 -0700

    fetching api

---
+++ src/api.js
@@ -0,0 +1,33 @@
+const base_url = "https://api.rawg.io/api/";
+const api_key = "?key=85e23c484944429da83905c969cd7a34&";
+//Getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+//Getting the date
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}${api_key}`;
